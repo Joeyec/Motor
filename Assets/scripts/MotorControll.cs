2455209a03b12a809a8af86e37b2b572ed03b91d
Joeyec <1329097376@qq.com>
2019-09-19 11:05:18 +0800

    push Motor

---
+++ Assets/scripts/MotorControll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotorControll : MonoBehaviour {
+    public WheelJoint2D right;
+    public WheelJoint2D left;
+    // Use this for initialization
+    [SerializeField] float speed = 1500f;
+    [SerializeField] float rotate = 1500f;
+    Rigidbody2D rg;
+    JointMotor2D motor;
+    void Start () {
+        motor.maxMotorTorque = 10000;
+        rg = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        motor.motorSpeed = -speed * Input.GetAxis("Horizontal");
+        right.motor = motor;
+        left.motor = motor;
+        rg.AddTorque(rotate * Input.GetAxis("Vertical")*Time.deltaTime);
+	}
+}
---
+++ Assets/scripts/camerafollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour {
+    public Transform target;
+    Vector3 camera;
+	// Use this for initialization
+	void Start () {
+        camera = target.position;
+        camera.z = -10;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camera = target.position;
+        camera.z = -10;
+        transform.position = camera;
+
+    }
+}
